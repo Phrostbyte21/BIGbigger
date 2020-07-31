4b1d01d4222277d5249c30bab7e80ba4a6c51661
phrostbyte21 <kevin@TL.thrivelife.com>
2020-07-31 11:12:58 -0600

    Added another testimonial

--- testimonials.js
+++ testimonials.js
@@ -4,5 +4,8 @@ export const testimonials = [
     },
     {
         "testimonial": "I have always been the idea starter, but never the finisher. When I was young I opened up a lemonade stand with my friends, and we had a lot of fun doing it! The problem was we never planned out how much lemonade actually costs to make, and did not set the price accordingly. Now as an adult, on a much larger scale, I have tons of great ideas with lots of holes in them. With BIGbigger, I came with my great ideas, and the gaps were filled in by experienced individuals, ready to make my dreams come true!"
+    },
+    {
+        "testimonial": "You know that feeling when you have a great idea, you write it down to make sure you don't forget, and you get really excited by just how awesome it is? And then a week later, you find the note, and laugh to yourself because you realize how unfinished your thoughts were. I've got notebooks FULL of ideas. Thanks to BIGbigger, I've seen some of those ideas actually come to life, and make me money! I couldn't have accomplished what I have without the help BIGbigger has given me."
     }
 ]
