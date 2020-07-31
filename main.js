643896706bdc754f32e7cf8eba3888ead2d83d21
phrostbyte21 <kevin@TL.thrivelife.com>
2020-07-30 21:30:11 -0600

    Getting random user imgs and names (hopefully)

--- main.js
+++ main.js
@@ -9,3 +9,32 @@ const q = document.createElement('q');
 q.innerText = testimonials[index].testimonial;
 
 document.getElementById('testimonial').append(q);
+
+//Random user images and names
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+const userURL = 'https://randomuser.me/api/';
+const div = document.getElementById('rndUser');
+let = users;
+
+fetch(userURL)
+    .then(resp => resp.json())
+    .then(data => {
+        users = data.results;
+        console.log(users)
+    })
+
+    let h1 = createNode(h1),
+        img = createNode(img);
+
+    img.src = users[0].picture.large;
+    h1.innerText = `${users.name.first} ${users.name.first}`;
+
+    append(h1, img);
+    append(h1, div)
