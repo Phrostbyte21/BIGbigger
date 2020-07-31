d70eefde64f959483ee215b33b9f1a03458e2de0
phrostbyte21 <kevin@TL.thrivelife.com>
2020-07-30 20:37:31 -0600

    making the API bigger

--- testimonials.js
+++ testimonials.js
@@ -4,5 +4,32 @@ export const testimonials = [
     },
     {
         "testimonial": "I have always been the idea starter, but never the finisher. When I was young I opened up a lemonade stand with my friends, and we had a lot of fun doing it! The problem was we never planned out how much lemonade actually costs to make, and did not set the price accordingly. Now as an adult, on a much larger scale, I have tons of great ideas with lots of holes in them. With BIGbigger, I came with my great ideas, and the gaps were filled in by experienced individuals, ready to make my dreams come true!"
+    },
+    {
+        "testimonial": "2"
+    },
+    {
+        "testimonial": "3"
+    },
+    {
+        "testimonial": "4"
+    },
+    {
+        "testimonial": "5"
+    },
+    {
+        "testimonial": "6"
+    },
+    {
+        "testimonial": "7"
+    },
+    {
+        "testimonial": "8"
+    },
+    {
+        "testimonial": "9"
+    },
+    {
+        "testimonial": "10"
     }
 ]
