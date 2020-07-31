4651258c1c6a8bdb156a697258605e9b9da54bdf
phrostbyte21 <kevin@TL.thrivelife.com>
2020-07-30 20:59:07 -0600

    stuff and things?

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-//Import Testimonials API
 import { testimonials } from './testimonials.js'
 
+const index = Math.floor(Math.random() * testimonials.length);
+
+const q = document.createElement('q');
+
+q.innerText = testimonials[index].testimonial;
+
+document.getElementById('testimonial').append(q);
+
+/*
+//Import Testimonials API
+import { testimonials } from './testimonials.js'
 
 function createNode(element) {
     return document.createElement(element);
@@ -10,8 +20,8 @@ function append(parent, el) {
     return parent.appendChild(el);
 }
 // Generates random number between 1 and 10.
-const testRndNum = Math.floor(Math.random() * (/*max*/10 - /*min*/1 + 1)) + /*min*/1;
-console.log(testRndNum);
+const index = Math.floor(Math.random() * testimonials.length);
+console.log(index);
 
 function first(array, count = 1) {
     return array.slice(0, count);
@@ -25,9 +35,10 @@ const testimonialQuote = document.getElementById('testimonial');
 first(testimonials).map(person => {
     let q = createNode('q')
 
-    // q.innerText = `${person.testimonial[testRndNum]}`;
+    q.innerText = `${person.testimonial}`;
 
     q.innerText = testimonial[testRndNum];
 
     append(testimonialQuote, q);
 })
+*/
