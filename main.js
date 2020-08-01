03ac7ed7f0d969db64eec5671eefaf0620519751
phrostbyte21 <kevin@TL.thrivelife.com>
2020-07-31 18:08:04 -0600

    making things more mobile friendly

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 import { testimonials } from './testimonials.js'
 
 // Create variable with random length based on API entries
+// then print the selected testimonial to HTML
 const index = Math.floor(Math.random() * testimonials.length);
 
 const q = document.createElement('q');
@@ -10,7 +11,7 @@ q.innerText = testimonials[index].testimonial;
 
 document.getElementById('testimonial').append(q);
 
-//Random user images and names
+//Pull random user images and names from the API
 const userURL = 'https://randomuser.me/api/';
 const span = document.getElementById('rndUser');
 let bigUsers;
@@ -19,7 +20,6 @@ fetch(userURL)
     .then(resp => resp.json())
     .then(data => {
         bigUsers = data.results;
-        console.log(bigUsers)
 
         bigUsers.map(user => {
             let h1 = document.createElement('h1'),
@@ -33,43 +33,8 @@ fetch(userURL)
         })
     })
 
-//Change Submit button text
+// Change Submit button text
 document.getElementById("submitBtn").addEventListener("click", () => {
     document.getElementById("submitBtn").innerHTML = "Submitted!";
     setTimeout(function(){ document.getElementById("submitBtn").innerHTML = "Submit" }, 5000);
 });
-
-
-
-//Working, just in case
-/*
-function createNode(element) {
-    return document.createElement(element);
-}
-
-function append(parent, el) {
-    return parent.appendChild(el);
-}
-
-const userURL = 'https://randomuser.me/api/';
-const span = document.getElementById('rndUser');
-let bigUsers;
-
-fetch(userURL)
-    .then(resp => resp.json())
-    .then(data => {
-        bigUsers = data.results;
-        console.log(bigUsers)
-
-        bigUsers.map(user => {
-            let h1 = createNode('h1'),
-                img = createNode('img');
-
-            img.src = user.picture.large;
-            h1.innerText = `${user.name.first} ${user.name.last}`;
-
-            append(span, img);
-            append(span, h1);
-        })
-    })
-*/
