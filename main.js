d2ba0233b731353b617a5ab4cecebf8100756035
phrostbyte21 <kevin@TL.thrivelife.com>
2020-08-04 11:29:51 -0600

    this should work than, right?

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ foodToggle.addEventListener('click', () => {
     businessDiv.innerText = '';
     
     business.filter(person => {
-        if (person.founded == '2017') {
+        if (person.types[0] == 'Food') {
             return person.name;
         }
     })
