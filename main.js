3115c58362e6b7b00e247f2c324d4cc6d7aae0ec
phrostbyte21 <kevin@TL.thrivelife.com>
2020-08-04 12:03:05 -0600

    janky first attempt at next test btn

--- main.js
+++ main.js
@@ -111,6 +111,42 @@ first(business).map(person => {
     createBusiness(person);
 })
 
+//Generates a new company testimonial
+const newTest = document.getElementById('newTestinomial');
+
+allToggle.addEventListener('click', () => {
+    const index = Math.floor(Math.random() * testimonials.length);
+
+    const q = document.createElement('q');
+    
+    q.innerText = testimonials[index].testimonial;
+    
+    document.getElementById('testimonial').append(q);
+    
+    //Pull random user images and names from the API
+    const userURL = 'https://randomuser.me/api/';
+    const span = document.getElementById('rndUser');
+    let bigUsers;
+    
+    fetch(userURL)
+        .then(resp => resp.json())
+        .then(data => {
+            bigUsers = data.results;
+    
+            bigUsers.map(user => {
+                let h1 = document.createElement('h1'),
+                    img = document.createElement('img');
+    
+                img.src = user.picture.large;
+                h1.innerText = `${user.name.first} ${user.name.last}`;
+    
+                span.appendChild(img);
+                span.appendChild(h1);
+            })
+        })
+})
+
+
 
 // Change Submit button text
 document.getElementById("submitBtn").addEventListener("click", () => {
